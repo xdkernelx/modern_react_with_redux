1717a8c0755c4525f1f45a0f36f84218c4c5e96d
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-28 04:52:24 -0800

    Successfully showcase a default array of videos.

--- redux_simple_starter/src/components/video_list_item.js
+++ redux_simple_starter/src/components/video_list_item.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return  <li>Video</li>
-}
+const VideoListItem = ({video}) => {
+  // You can clean the following by adding
+  // the field needed to modify in curly braces
+  // and placing it on top.
+  //const video = props.video;
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return  <li className="list-group-item">
+            <div className="video-list media">
+              <div className="media-left">
+                <img className="media-object" src={imageUrl} />
+              </div>
+
+              <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+              </div>
+            </div>
+          </li>
+};
 
 export default VideoListItem;
