24aa6a05e8298282dc3295de87dc73bc5799a164
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-28 04:07:44 -0800

    Cool I guess, just a way to showcase current state of a class. I am liking the classes in ReactJS though.

--- redux_simple_starter/src/components/search_bar.js
+++ redux_simple_starter/src/components/search_bar.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
-  
 
-  render() {
-    return <input />;
+  constructor(props) {
+    super(props);
+
+
+    // You can only change the state like this only here
+    this.state = { term: 'Starting value' };
   }
+  
+  // When this changes, run specified method
+  // This basically prints out what you are 
+  // typing into the console
+  render() {
+    return  <div>
+              <input value={this.state.term} onChange={event => this.setState({ term : event.target.value })} />;
+            </div>
+  }
+
 }
 
 export default SearchBar;
