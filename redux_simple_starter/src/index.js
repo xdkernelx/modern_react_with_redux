8ea86d6a29b3a16056f16929cdbaa6a6261530f1
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-28 02:54:53 -0800

    Add several files that will become the individual components.

--- redux_simple_starter/src/index.js
+++ redux_simple_starter/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = "AIzaSyB_y41M_1_Kx2sFdzXuRqWOOcqketLWWA0";
+
+
 // Create a new component that produces HTML
 // Const is ES6 similar to var
 // JSX is a HTML-like JS
+// Golden rule is one file per container
 
 const App = () => {
 
