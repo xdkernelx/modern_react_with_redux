82c190015b1d31f989ff59aa6b718774e5a0f147
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-28 05:14:28 -0800

    Add a state field to the App controller.

--- redux_simple_starter/src/index.js
+++ redux_simple_starter/src/index.js
@@ -21,21 +21,27 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    };
 
 
     // When the incoming parameter is coming in
     // You can omit the this.val : val syntax and just
     // have val inside curly brackets
     YTSearch({key: API_KEY, term : 'surfboards'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   };
 
   render() {
     return <div>
             <SearchBar />
-            <VideoDetail video={this.state.videos[0]} />
+            <VideoDetail video={this.state.selectedVideo} />
             <VideoList videos={this.state.videos} />
            </div>;
   };
